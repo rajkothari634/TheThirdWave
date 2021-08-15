453ba066ef1dc9e300806bc10b0f78b6c6a4dd07
Raj kothari <rkmeansrk39@gmail.com>
2021-08-15 19:59:09 +0530

    update

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const envJson = require("dotenv").config();
 
 //connecting mongoDB server
 const databaseString = process.env.DATABASE_PRODUCTION;
-const password =  process.env.DATABASE_PRODUCTION_PASSWORD;
+const password =   process.env.DATABASE_PRODUCTION_PASSWORD;
 const DB = databaseString.replace("<PASSWORD>", password);
 mongoose
   .connect(DB, {
